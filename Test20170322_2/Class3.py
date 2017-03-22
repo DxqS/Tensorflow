f943ae65fe24e604758d982577daecc44ebe3ead
dxq02 <353622088@qq.com>
2017-03-22 03:11:39 -0700

    class4

--- Test20170322_2/Class3.py
+++ Test20170322_2/Class3.py
@@ -4,8 +4,8 @@ Created on 3/21/17.
 
 @author: dxq
 '''
-import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 sess = tf.InteractiveSession()
